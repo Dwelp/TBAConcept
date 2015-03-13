d67c0c99976eb7f7017443b1640f177453b46749
Dwelp <gabriel.chirila.ro@gmail.com>
2015-03-13 22:01:51 +0200

    More third party tools
    Some utility scripts
    Proto assets

---
+++ TBA Concept Prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : AutoSingletonManager<GameManager>
+{
+    //Initialization code (called from Awake) goes in here
+    public override void InitManager()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
---
+++ TBA Concept Prototype/Assets/Scripts/QTimer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+ 
+public class QTimer
+ 
+{
+	/// <summary>
+	/// Constructor 
+	/// </summary>
+    public QTimer()
+    {
+        On = false;
+        RecTime = 0.0f;
+    }
+ 
+	/// <summary>
+	/// Checks if timer is running 
+	/// </summary>
+    public bool On { get; set; }
+ 
+	/// <summary>
+	/// Startup time 
+	/// </summary>
+    public float RecTime { get; set; }
+ 
+	/// <summary>
+	/// Start timer 
+	/// </summary>
+ 
+    public void Reset()
+    {
+        RecTime = Time.time;
+        On = true;
+    }
+ 
+	/// <summary>
+	/// Stop timer 
+	/// </summary>
+    public void Stop()
+    {
+        On = false;
+        RecTime = 0.0f;
+    }
+ 
+	/// <summary>
+	/// Check difference between current time and startup time 
+	/// </summary>
+    public float Difference()
+    {
+        return Time.time - RecTime;
+    }
+}
